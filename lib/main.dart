a271b02ab24804732765b97e2f33aaae7d21d5d2
devMilena07 <britessantanamilena@gmail.com>
2025-07-23 21:57:15 -0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: SafeArea(
-          child: WebView(
-            initialUrl: 'https://mobile.melfitness.com.br/',
-            javascriptMode: JavascriptMode.unrestricted,
-          ),
-        ),
+      home: WebViewContainer(),
+    );
+  }
+}
+
+class WebViewContainer extends StatefulWidget {
+  const WebViewContainer({super.key});
+
+  @override
+  State<WebViewContainer> createState() => _WebViewContainerState();
+}
+
+class _WebViewContainerState extends State<WebViewContainer> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://mobile.melfitness.com.br/'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
       ),
     );
   }
